16ca9d58cfcde3b95cd456cc9bcdc9aaefffffbb
michaelsuzuki <michael_t_suzuki@yahoo.com>
2022-11-17 15:24:06 -0800

    Add support for running off cmd line (#10)

--- src/memkv/client/cli.py
+++ src/memkv/client/cli.py
@@ -1,7 +1,8 @@
 import logging
 import re
 import shlex
-from typing import Dict, List
+import sys
+from typing import Callable, Dict, Final, List, Union, Type
 
 import click
 from prompt_toolkit import PromptSession, print_formatted_text
@@ -10,16 +11,20 @@ from prompt_toolkit.history import InMemoryHistory
 
 from memkv.client.api import Client
 
-version = "0.1"
+PARSED_ARGS_T: Final[Type] = Union[List[str], Dict[str, bytes]]
 
-log_format = "%(asctime)s::%(levelname)s::%(name)s::%(filename)s::%(lineno)d::%(message)s"
+version: Final[str] = "0.1"
+
+log_format: Final[str] = \
+    "%(asctime)s::%(levelname)s::%(name)s::%(filename)s::%(lineno)d::%(message)s"
+
 logging.basicConfig(
     level="INFO",
     format=log_format
 )
-logger = logging.getLogger(__name__)
+logger: Final[str] = logging.getLogger(__name__)
 
-cmd_pat = re.compile(r"^\s*(GET|SET|DELETE|METRICS)\s*", re.IGNORECASE)
+cmd_pat: Final[re.Pattern] = re.compile(r"^\s*(GET|SET|DELETE|METRICS)\s*", re.IGNORECASE)
 
 
 class MisMatchedArgsError(Exception):
@@ -49,8 +54,7 @@ def get_key_value_dict(args=List[str]) -> Dict[str, bytes]:
     }
 
 
-def execute_get(client: Client, session: PromptSession, args: str) -> None:
-    keys = shlex.split(args)
+def execute_get(client: Client, keys: List[str]) -> None:
     try:
         key_values = client.get(keys)
         for key, value in key_values.items():
@@ -59,17 +63,29 @@ def execute_get(client: Client, session: PromptSession, args: str) -> None:
         print_formatted_text(f"Error retrieving values for keys: {e}")
 
 
-def execute_set(client: Client, session: PromptSession, args: str) -> None:
-    keys_and_values = get_key_value_dict(shlex.split(args, posix=True))
+def parse_args_string(
+    client: Client, args_str: str, parse_fn: Callable[[str], PARSED_ARGS_T]
+) -> PARSED_ARGS_T:
+    return parse_fn(args_str)
+
+
+def parse_keys(args_str: str) -> List[str]:
+    return shlex.split(args_str)
+
+
+def parse_key_values(args_str: str) -> Dict[str, bytes]:
+    return get_key_value_dict(shlex.split(args_str, posix=True))
+
+
+def execute_set(client: Client, key_values: Dict[str, bytes]) -> None:
     try:
-        keys_set = client.set(key_values=keys_and_values)
+        keys_set = client.set(key_values=key_values)
         print_formatted_text(f"Updated: {', '.join(keys_set)}")
     except Exception as e:
         print_formatted_text(f"Error adding/updating keys: {e}")
 
 
-def execute_delete(client: Client, session: PromptSession, args: str):
-    keys = shlex.split(args)
+def execute_delete(client: Client, keys: List[str]):
     try:
         client.delete(keys)
         print_formatted_text(f"Successfully deleted these keys: {keys}")
@@ -77,7 +93,7 @@ def execute_delete(client: Client, session: PromptSession, args: str):
         print_formatted_text(f"Error deleting keys from store {e}")
 
 
-def execute_metrics(client: Client, session: PromptSession):
+def execute_metrics(client: Client):
     try:
         metrics = client.metrics()
         print_formatted_text("SERVER METRICS:")
@@ -95,7 +111,7 @@ def should_continue(session: PromptSession, message: str) -> bool:
     return answer[0].lower() == "y"
 
 
-def get_required_args(args: List[str]) -> str:
+def get_args_string(args: List[str]) -> str:
     if len(args) > 1:
         return args[1]
     raise NoArgsFoundError(
@@ -107,13 +123,13 @@ def process_input(ctx, session: PromptSession, input: str, client: Client):
     cmd_and_args = input.split(" ", 1)
     cmd = cmd_and_args[0].strip().upper()
     if cmd == "GET":
-        execute_get(client, session, get_required_args(cmd_and_args))
+        execute_get(client, parse_args_string(get_args_string(cmd_and_args), parse_keys))
     elif cmd == "SET":
-        execute_set(client, session, get_required_args(cmd_and_args))
+        execute_set(client, parse_args_string(get_args_string(cmd_and_args), parse_key_values))
     elif cmd == "DELETE":
-        execute_delete(client, session, get_required_args(cmd_and_args))
+        execute_delete(client, parse_args_string(get_args_string(cmd_and_args), parse_keys))
     elif cmd == "METRICS":
-        execute_metrics(client, session)
+        execute_metrics(client)
     elif cmd in ("QUIT", "Q"):
         if should_continue(session, "Are you sure you want to quit?"):
             exit(0)
@@ -125,6 +141,21 @@ def process_input(ctx, session: PromptSession, input: str, client: Client):
                 f"Unknown command entered: {cmd}. \
                     You can enter 'help' for more information or try again"
             )
+
+
+def process_cmd_with_args(client: Client, cmd: str, args: PARSED_ARGS_T):
+    if cmd.upper() == "GET":
+        execute_get(client, args)
+    elif cmd.upper() == "SET":
+        execute_set(client, get_key_value_dict(args))
+    elif cmd.upper() == "DELETE":
+        execute_delete(client, args)
+    elif cmd.upper() == "METRICS":
+        execute_metrics(client)
+    else:
+        print_formatted_text(f"Error: Unable to execute unknown command {cmd}")
+        exit(-1)
+    exit(0)
 
 
 @click.command()
@@ -143,8 +174,12 @@ def process_input(ctx, session: PromptSession, input: str, client: Client):
 @click.option(
     "--debug", is_flag=True, default=False, help="Set this if you want more verbose logging"
 )
+@click.argument(
+    "cmd", nargs=1, type=click.Choice(["GET", "SET", "DELETE", "METRICS"], case_sensitive=False)
+)
+@click.argument("args", nargs=-1, type=click.UNPROCESSED)
 @click.pass_context
-def main(ctx, host: str, port: int, debug: bool):
+def main(ctx, host: str, port: int, cmd: str, args: List[Union[str, bytes]], debug: bool):
     """This cli allows one to interact with a memkv server.
 
     You may interact the server by starting the client and sending commands
@@ -179,11 +214,18 @@ def main(ctx, host: str, port: int, debug: bool):
     if debug:
         logger.setLevel("DEBUG")
 
-    session = new_session()
     client = Client(host=host, port=port)
+    if cmd is not None:
+        print(f"GOT command: {cmd}")
+        process_cmd_with_args(client, cmd, args)
+
+    session = new_session()
     while True:
-        input = session.prompt("> ")
-        process_input(ctx, session, input, client)
+        try:
+            input = session.prompt("> ")
+            process_input(ctx, session, input, client)
+        except Exception as e:
+            print_formatted_text(f"Error executing command: {e}")
 
 
 if __name__ == "__main__":
